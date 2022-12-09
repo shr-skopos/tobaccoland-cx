45226bbb3d1121e483cb6ee3cf2b4a716e74a3ab
shr-skopos <saied.hosseinian@skopos-connect.de>
2022-12-09 12:36:48 +0100

    add background-image style

--- src/animateSmiley.js
+++ src/animateSmiley.js
@@ -1,9 +1,5 @@
 export default function eanCodeScan() {
 
-const MAIN_CONTAINER = document.querySelector("#mainContainer");
-
-
-MAIN_CONTAINER.innerHTML = "Guten Tag Mr. Haupt ..!";
 console.log("Guten Tag Mr. Haupt ..!");
 
 }
