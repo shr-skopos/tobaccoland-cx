e4243628611f396fa753d40a557cf477194db9e9
shr-skopos <saied.hosseinian@skopos-connect.de>
2022-12-12 14:30:54 +0100

    add speech bubble (example)

--- src/animateSmiley.js
+++ src/animateSmiley.js
@@ -1,5 +1,39 @@
-export default function eanCodeScan() {
+export default function animateSmiley() {
 
-console.log("Guten Tag Mr. Haupt ..!");
+    // elements
+    const SURVEYENGINEBODY = document.querySelector("#SurveyEngineBody");
+    const INNERINNER = document.querySelector(".InnerInner");
+    const QUESTIONTEXT = document.querySelector(".InnerInner .QuestionText");
 
+      // build views
+      const buildSpeechBubble = () => {
+        
+        let speechBubble = createEl("div");
+        speechBubble.className = "speechBubble";
+
+        let topAboveSpeechBubble = createEl("div");
+        topAboveSpeechBubble.className = "topAboveSpeechBubble";
+
+        let centerAboveSpeechBubble = createEl("div");
+        centerAboveSpeechBubble.className = "centerAboveSpeechBubble";
+
+        let bottomAboveSpeechBubble = createEl("div");
+        bottomAboveSpeechBubble.className = "bottomAboveSpeechBubble";
+
+        centerAboveSpeechBubble.appendChild(QUESTIONTEXT);
+        speechBubble.append(topAboveSpeechBubble,centerAboveSpeechBubble,bottomAboveSpeechBubble);
+        INNERINNER.appendChild(speechBubble);
+    }
+    buildSpeechBubble();
+
+
+    // helper functions
+    function createEl(tag, txt){
+        let el = document.createElement(tag);
+        if (txt) {
+            txt = document.createTextNode(txt);
+            el.appendChild(txt);
+        }
+        return el;
+    }
 }
