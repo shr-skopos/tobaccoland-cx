8afd6acfe6b6c458cd8cc91274bd06e3bf03e9b6
shr-skopos <saied.hosseinian@skopos-connect.de>
2022-12-15 13:11:58 +0100

    add animation lef Eye

--- src/animateSmiley.js
+++ src/animateSmiley.js
@@ -1,29 +1,49 @@
-export default function animateSmiley() {
+export default function animateSmiley(page) {
 
     // elements
     const QUESTIONS = document.querySelector("#Questions");
     const SKINCONTENT = document.querySelector("#SkinContent");
+    const BUTTONS = document.querySelector("#Buttons");
+    const START = document.querySelector("#Questions.start");
+    const END = document.querySelector("#Questions.end");
 
-      // build views
-      const buildSpeechBubble = () => {
+    // build views
+    const buildSpeechBubble = () => {
 
-        let topSpeechBubble = createEl("div");
+        const topSpeechBubble = createEl("div");
         topSpeechBubble.className = "topSpeechBubble";
 
         QUESTIONS.className = "centerSpeechBubble";
 
-        let bottomSpeechBubble = createEl("div");
+        const bottomSpeechBubble = createEl("div");
         bottomSpeechBubble.className = "bottomSpeechBubble";
-        
+
         SKINCONTENT.prepend(topSpeechBubble);
-        Buttons.insertAdjacentElement("beforebegin", bottomSpeechBubble);
+        BUTTONS.insertAdjacentElement("beforebegin", bottomSpeechBubble);
 
     }
     buildSpeechBubble();
 
+    const buildWinkEWA = () => {
+        QUESTIONS.classList.add(page);
+    }
+    buildWinkEWA();
+
+    const buildBlinkingEye = () => {
+
+        const leftEye = createEl("div");
+        leftEye.className = "leftEye";
+
+        const eyeLid = createEl("div");
+        eyeLid.className = "eyeLid";
+
+        leftEye.appendChild(eyeLid);
+        SKINCONTENT.appendChild(leftEye);
+    }
+    buildBlinkingEye();
 
     // helper functions
-    function createEl(tag, txt){
+    function createEl(tag, txt) {
         let el = document.createElement(tag);
         if (txt) {
             txt = document.createTextNode(txt);
